a7d0a03b08190a50a73cd9a2462c21373baa307c
WataruMiyamoto <watarumiyamoto.tech@gmail.com>
2021-01-05 18:37:43 +0900

    Gov_topicモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_090707) do
+ActiveRecord::Schema.define(version: 2021_01_05_093319) do
 
   create_table "drafts", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,25 @@ ActiveRecord::Schema.define(version: 2021_01_05_090707) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_drafts_on_user_id"
+  end
+
+  create_table "gov_topics", force: :cascade do |t|
+    t.string "gov_name"
+    t.string "title"
+    t.string "period"
+    t.string "content"
+    t.string "target"
+    t.string "counter"
+    t.string "link"
+    t.boolean "emergency", default: false, null: false
+    t.boolean "primary_sector", default: false, null: false
+    t.boolean "other_sector", default: false, null: false
+    t.boolean "emigration", default: false, null: false
+    t.boolean "senior", default: false, null: false
+    t.boolean "parenting", default: false, null: false
+    t.boolean "other", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
